c17857e48a786ce58488d660737afc0370cc53f8
xinGGG <317033640@qq.com>
2015-11-30 13:52:47 +0800

    建立项目
    提交

---
+++ weibo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weibo
+//
+//  Created by Dragonpass on 15/11/30.
+//  Copyright © 2015年 xinGGG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
